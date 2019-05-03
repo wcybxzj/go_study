1a6ab51f185b36f6113e23b1202b0c3faa1546bd
Your Name <you@example.com>
2019-05-03 15:20:51 +0800

    finish reverseLinklist

--- 15.leetcode/1.twoSum.go
+++ 15.leetcode/1.twoSum.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+//https://leetcode.com/problems/two-sum/
+//https://leetcode-cn.com/problems/two-sum/
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+//你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+
 //方法1:暴力循环
 func twoSumV1(nums []int, target int) []int {
 	var ret []int
@@ -17,8 +23,31 @@ func twoSumV1(nums []int, target int) []int {
 	return ret
 }
 
-//方法2:hash
+
+//方法2:two-way-hash
 func twoSumV2(nums []int, target int) []int {
+	hash1_map := make(map[int]int)
+	//hash2_map := make(map[int]int)
+
+	for k, v :=range nums {
+		hash1_map[v] = k
+	}
+
+	for k, v :=range nums {
+		tmp := target-v
+		k1, ok := hash1_map[tmp]
+		if ok && k != k1{
+			return []int{k ,k1}
+		}
+	}
+
+	return []int{}
+}
+
+
+
+//方法3:one-way-hash
+func twoSumV3(nums []int, target int) []int {
 	var ret []int
 	hash_map := make(map[int]int)
 	for i, x := range nums {
@@ -34,7 +63,7 @@ func twoSumV2(nums []int, target int) []int {
 	return ret
 }
 
-//https://leetcode.com/problems/two-sum/
+
 func main() {
 	var ret []int
 	arr1 := []int{2, 7, 11, 15}
@@ -43,5 +72,8 @@ func main() {
 	fmt.Println(ret)
 
 	ret = twoSumV2(arr1, 9)
+	fmt.Println(ret)
+
+	ret = twoSumV3(arr1, 9)
 	fmt.Println(ret)
 }
--- 15.leetcode/2.reverseLinkedList.go
+++ 15.leetcode/2.reverseLinkedList.go
@@ -2,44 +2,80 @@ package main
 
 import "fmt"
 
+//https://leetcode.com/problems/reverse-linked-list/solution/
+//https://leetcode-cn.com/problems/reverse-linked-list/
+
+//反转一个单链表。
+//示例:
+//输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL
+//进阶:
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
-//func reverseList(head *ListNode) *ListNode {
-//
-//}
+//方法1:循环版
+func reverseList1(head *ListNode) *ListNode {
+	cur:= head
+	var prev *ListNode = nil
 
-func create(arr1 []int) *ListNode {
-	var prev *ListNode
-	var head ListNode
-	for index, value := range arr1 {
-		//fmt.Println(index, value)
-		if index == 0 {
-			head := ListNode{Val: value, Next: nil}
-			prev = &head
-		} else {
-			node := &ListNode{Val: value, Next: nil}
-			prev.Next = node
-			prev = node
-		}
+	for ;cur!=nil;  {
+		tmpcur := cur.Next
+		cur.Next= prev
+		prev = cur
+		cur = tmpcur
 	}
-	return &head
+	return prev
+}
+
+
+//方法1:递归版
+func reverseList2(head *ListNode) *ListNode {
+
+	if head ==nil || head.Next==nil{
+		return head
+	}
+	p := reverseList2(head.Next)
+	head.Next.Next = head
+	head.Next=nil
+	return p
 }
 
 func travse(node *ListNode) {
-	for (*node).Next != nil {
-		fmt.Println(node.Val)
+	for ;node!=nil; {
+		fmt.Print(node.Val)
 		node = node.Next
 	}
+	fmt.Println()
 }
 
-//没写完去学nginx了
 //Input: 1->2->3->4->5->NULL
 //Output: 5->4->3->2->1->NULL
 func main() {
-	arr1 := []int{1, 2, 3, 4, 5}
-	head := create(arr1)
+	var head *ListNode
+	n5 := ListNode{5, nil}
+	n4 := ListNode{4, &n5}
+	n3 := ListNode{3, &n4}
+	n2 := ListNode{2, &n3}
+	n1 := ListNode{1, &n2}
+
+	travse(&n1)
+	head = reverseList1(&n1)
 	travse(head)
+
+
+	n5 = ListNode{5, nil}
+	n4 = ListNode{4, &n5}
+	n3 = ListNode{3, &n4}
+	n2 = ListNode{2, &n3}
+	n1 = ListNode{1, &n2}
+
+	travse(&n1)
+	head = reverseList2(&n1)
+	travse(head)
+
+
 }
